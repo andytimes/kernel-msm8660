660c19426beea61f1840bc9ae736b0a300c721b6
Andy Deng <theandy.deng@gmail.com>
2015-09-01 11:37:49 +0800

    acpuclock: Enable CPU overclock andy tune GPU Min Freq
    Signed-off-by: Andy Deng <theandy.deng@gmail.com>

--- lge/lge_board/ijb_skt/devices_ijb_skt.c
+++ lge/lge_board/ijb_skt/devices_ijb_skt.c
@@ -878,7 +878,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 50,
 		},
 		{
-			.gpu_freq = 145455000,
+			.gpu_freq = 177778000,
 			.bus_freq = 1,
 			.io_fraction = 100,
 		},
