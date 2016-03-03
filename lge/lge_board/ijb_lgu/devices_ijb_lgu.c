07fbf0849a6291a8cce8170f5c888d21e09eb74f
Andy Deng <theandy.deng@gmail.com>
2016-03-03 13:14:00 +0800

    lge/gpu: don't gpu overclocking
    gpu 2D case much of battery consume, for battery saving,
    don't using unstable high-freq.

--- lge/lge_board/ijb_lgu/devices_ijb_lgu.c
+++ lge/lge_board/ijb_lgu/devices_ijb_lgu.c
@@ -699,7 +699,7 @@ static struct msm_bus_vectors grp3d_low_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(1000),
+		.ib = KGSL_CONVERT_TO_MBPS(990),
 	},
 };
 
@@ -708,7 +708,7 @@ static struct msm_bus_vectors grp3d_nominal_low_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2048),
+		.ib = KGSL_CONVERT_TO_MBPS(1300),
 	},
 };
 
@@ -717,7 +717,7 @@ static struct msm_bus_vectors grp3d_nominal_high_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2484),
+		.ib = KGSL_CONVERT_TO_MBPS(2008),
 	},
 };
 
@@ -726,7 +726,7 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2976),
+		.ib = KGSL_CONVERT_TO_MBPS(2484),
 	},
 };
 
@@ -773,7 +773,7 @@ static struct msm_bus_vectors grp2d0_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(1536),
+		.ib = KGSL_CONVERT_TO_MBPS(990),
 	},
 };
 
@@ -808,7 +808,7 @@ static struct msm_bus_vectors grp2d1_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(1536),
+		.ib = KGSL_CONVERT_TO_MBPS(990),
 	},
 };
 
@@ -863,14 +863,14 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 320000000,
+			.gpu_freq = 266667000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
 		{
-			.gpu_freq = 266667000,
+			.gpu_freq = 228571000,
 			.bus_freq = 3,
-			.io_fraction = 0,
+			.io_fraction = 33,
 		},
 		{
 			.gpu_freq = 200000000,
@@ -930,7 +930,7 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 			.bus_freq = 1,
 		},
 		{
-			.gpu_freq = 27000000,
+			.gpu_freq = 200000000,
 			.bus_freq = 0,
 		},
 	},
@@ -977,7 +977,7 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 			.bus_freq = 1,
 		},
 		{
-			.gpu_freq = 27000000,
+			.gpu_freq = 200000000,
 			.bus_freq = 0,
 		},
 	},
